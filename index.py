5376de740e637b13da8091f0a2dbaa19ac3d0c7d
Maizares, Juan <juan_maizares386@hotmail.com>
2020-04-02 17:04:30 -0300

    exportando a json

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import json
+
 datos = urllib.request.urlopen("http://www.cinemalaplata.com/").read().decode('latin1')
 
 soup =  BeautifulSoup(datos)
 tags = soup("img")
+data = {}
+data['peliculas'] = []
 for tag in tags:
-	print(tag.get("alt"))
+    data['peliculas'].append({
+        'name_film': tag.get("alt"),
+    })
+with open('data.json', 'w') as file:
+    json.dump(data, file, indent=3)
