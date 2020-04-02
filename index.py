0c5fae4d456ab797f53fa767b5753280556c9127
Maizares, Juan <juan_maizares386@hotmail.com>
2020-04-02 20:18:22 -0300

    toma la url de cada pelicula y toma info de cada una

--- index.py
+++ index.py
@@ -2,15 +2,37 @@ import urllib.request
 from bs4 import BeautifulSoup
 import json
 
-datos = urllib.request.urlopen("http://www.cinemalaplata.com/").read().decode('latin1')
+#metodo que recupera informacion de una pelicula y devuelve en forma de JSON
+def getInfoFilm(IdTitulo):
+    urlStr = "http://www.cinemalaplata.com/sinopsis.aspx?Seccion=FUTURO&IdTitulo=" + IdTitulo
+    datos = urllib.request.urlopen(urlStr).read().decode('latin1')
+    soup =  BeautifulSoup(datos)
+    titulos = soup.find_all("h4", {"class": "shortcodes-title"})
+    for titulo in titulos:
+        print(titulo.text.strip())
 
-soup =  BeautifulSoup(datos)
-tags = soup("img")
-data = {}
-data['peliculas'] = []
-for tag in tags:
-    data['peliculas'].append({
-        'name_film': tag.get("alt"),
-    })
-with open('data.json', 'w') as file:
-    json.dump(data, file, indent=3)
+def main():
+    datos = urllib.request.urlopen("http://www.cinemalaplata.com/").read().decode('latin1')
+
+    soup =  BeautifulSoup(datos)
+    tags = soup.find_all("div", {"class": "item1"})
+
+    for tag in tags:
+        var = tag.find(["a"])
+        s = var.get("href")
+        start = 'IdTitulo='
+        end = 'false'
+        inicio = s.split("IdTitulo=", 1)
+        var = 1
+        for ss in inicio:
+           if var == 2:
+                final = ss.split(",", 1)
+                url = ((final[0])[:-1])
+                getInfoFilm(url)
+           var=var+1
+     
+
+
+if __name__ == '__main__':
+    main()
+
